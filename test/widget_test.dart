9a2c9ada5490077fccc4bb789712acf5ad16d7cc
Vsevolod Lazuka <74600980+arigandores@users.noreply.github.com>
2025-06-04 17:14:21 +0200

    Replace default counter test with button presence test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -11,20 +11,13 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:haptic_feedback_app/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
+  testWidgets('MyApp shows key buttons', (WidgetTester tester) async {
+    // Build the app and trigger a frame.
     await tester.pumpWidget(const MyApp());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    // Verify that main action buttons are present.
+    expect(find.text('Success'), findsOneWidget);
+    expect(find.text('Warning'), findsOneWidget);
+    expect(find.text('Error'), findsOneWidget);
   });
 }
